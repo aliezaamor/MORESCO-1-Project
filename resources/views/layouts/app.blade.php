8496bec3bf9d4d6f6d2f454467955da44484e267
Alieza Amor R. Atienda <aliezaamor.atienda@g.msuiit.edu.ph>
2026-02-25 13:20:28 +0800

    BG

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SMS System</title>
+    <!-- Favicon -->
+    <link rel="icon" type="image/x-icon" href="{{ asset('images/M1.jpg') }}">
     <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
